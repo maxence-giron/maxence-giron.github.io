6535b05d5e087b57c38103de15a61533005970be
Max <gironmaxence.pro@gmail.com>
2023-01-04 14:58:03 +0100

    Delete public directory

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-function displayDiv(id) {
-    let div = document.getElementById(id);
-
-    if (div.style.display == 'none') {
-        div.style.display = 'block';
-    } else {
-        div.style.display = 'none';
-    }
-}
