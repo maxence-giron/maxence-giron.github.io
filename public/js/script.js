1fc5ac17dc39d4db2e64b3c37afa0176ad716196
Max <gironmaxence.pro@gmail.com>
2023-03-27 13:06:51 +0200

    Update my portfolio [v5.2] 🚀

--- public/js/script.js
+++ public/js/script.js
@@ -4,38 +4,38 @@
  * TOGGLE
  */
 const elemToggleFunc = function (elem) {
-    elem.classList.toggle("active")
+    elem.classList.toggle('active')
 }
 
 /**
  * GO TO TOP
  */
-const header = document.querySelector("[data-header]")
-const goTopBtn = document.querySelector("[data-go-top]")
+const header = document.querySelector('[data-header]')
+const goTopBtn = document.querySelector('[data-go-top]')
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
     if (window.scrollY >= 10) {
-        header.classList.add("active")
-        goTopBtn.classList.add("active")
+        header.classList.add('active')
+        goTopBtn.classList.add('active')
     } else {
-        header.classList.remove("active")
-        goTopBtn.classList.remove("active")
+        header.classList.remove('active')
+        goTopBtn.classList.remove('active')
     }
 })
 
 /**
  * NAVBAR TOGGLE
  */
-const navToggleBtn = document.querySelector("[data-nav-toggle-btn]")
-const navbar = document.querySelector("[data-navbar]")
+const navToggleBtn = document.querySelector('[data-nav-toggle-btn]')
+const navbar = document.querySelector('[data-navbar]')
 
-navToggleBtn.addEventListener("click", function () {
+navToggleBtn.addEventListener('click', function () {
     elemToggleFunc(navToggleBtn)
     elemToggleFunc(navbar)
     elemToggleFunc(document.body)
 })
 
-navbar.addEventListener("click", function () {
+navbar.addEventListener('click', function () {
     elemToggleFunc(navToggleBtn)
     elemToggleFunc(navbar)
     elemToggleFunc(document.body)
@@ -44,12 +44,12 @@ navbar.addEventListener("click", function () {
 /**
  * SKILLS TOGGLE
  */
-const toggleBtnBox = document.querySelector("[data-toggle-box]")
-const toggleBtns = document.querySelectorAll("[data-toggle-btn]")
-const skillsBox = document.querySelector("[data-skills-box]")
+const toggleBtnBox = document.querySelector('[data-toggle-box]')
+const toggleBtns = document.querySelectorAll('[data-toggle-btn]')
+const skillsBox = document.querySelector('[data-skills-box]')
 
 for (let i = 0; i < toggleBtns.length; i++) {
-    toggleBtns[i].addEventListener("click", function () {
+    toggleBtns[i].addEventListener('click', function () {
         elemToggleFunc(toggleBtnBox)
         for (let i = 0; i < toggleBtns.length; i++) {
             elemToggleFunc(toggleBtns[i])
@@ -61,30 +61,30 @@ for (let i = 0; i < toggleBtns.length; i++) {
 /**
  * THEME TOGGLE
  */
-const themeToggleBtn = document.querySelector("[data-theme-btn]")
+const themeToggleBtn = document.querySelector('[data-theme-btn]')
 
-themeToggleBtn.addEventListener("click", function () {
+themeToggleBtn.addEventListener('click', function () {
     elemToggleFunc(themeToggleBtn)
-    if (themeToggleBtn.classList.contains("active")) {
-        document.body.classList.remove("dark_theme")
-        document.body.classList.add("light_theme")
-        localStorage.setItem("theme", "light_theme")
+    if (themeToggleBtn.classList.contains('active')) {
+        document.body.classList.remove('dark')
+        document.body.classList.add('light')
+        localStorage.setItem('theme', 'light')
     } else {
-        document.body.classList.add("dark_theme")
-        document.body.classList.remove("light_theme")
-        localStorage.setItem("theme", "dark_theme")
+        document.body.classList.add('dark')
+        document.body.classList.remove('light')
+        localStorage.setItem('theme', 'dark')
     }
 })
 
 /**
  * CHECK THEME
  */
-if (localStorage.getItem("theme") === "light_theme") {
-    themeToggleBtn.classList.add("active")
-    document.body.classList.remove("dark_theme")
-    document.body.classList.add("light_theme")
+if (localStorage.getItem('theme') === 'light') {
+    themeToggleBtn.classList.add('active')
+    document.body.classList.remove('dark')
+    document.body.classList.add('light')
 } else {
-    themeToggleBtn.classList.remove("active")
-    document.body.classList.remove("light_theme")
-    document.body.classList.add("dark_theme")
+    themeToggleBtn.classList.remove('active')
+    document.body.classList.remove('light')
+    document.body.classList.add('dark')
 }
