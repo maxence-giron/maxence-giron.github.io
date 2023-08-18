e63fd3b4e84decbd1ff2affda38ffe4d4a71e005
Max <gironmaxence.pro@gmail.com>
2023-08-18 17:48:38 +0200

    Update my portfolio [v5.8] 🚀

--- public/js/script.js
+++ public/js/script.js
@@ -108,8 +108,8 @@ sendMail.addEventListener('click', function () {
         const mailtoLink = `mailto:gironmaxence.pro@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(template)}`
         window.location.href = mailtoLink
 
-        alert('Merci pour votre message.')
         window.location.href = 'https://maxhwk.github.io'
+        alert('Merci pour votre message.')
     } else {
         alert('Veuillez remplir les champs requis avant d\'envoyer votre message.')
     }
